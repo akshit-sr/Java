835c6572ed876edf7b2b46111e75bbfe680a24be
akshi-sr <akshit.sr2005@gmail.com>
2024-01-27 17:43:46 +0530

    Update Nested_Triangle.java

--- Loops/Nesting/Nested_Triangle.java
+++ Loops/Nesting/Nested_Triangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Nested_R_Triangle {
+public class Nested_Triangle {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
