d9488d4201e6989fa74b1782a9e3e501e2bd60e6
akshi-sr <139132127+akshi-sr@users.noreply.github.com>
2024-01-27 16:08:55 +0530

    Update Nested_Inv_R_Tri.java

--- Loops/Nesting/Nested_Inv_R_Tri.java
+++ Loops/Nesting/Nested_Inv_R_Tri.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Nested_Inv_R_Tri {
+public class Nested_Inv_Tri {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
